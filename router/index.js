34bf11fac62a46b4e4e1d74475f89c0a90aa2262
unknown <tmanimaran2005@gmail.com>
2025-09-21 17:47:54 +0530

    backend folder

---
+++ router/index.js
@@ -0,0 +1,16 @@
+// routers(is a server one of the path concept  )
+
+/*
+ * In Express, routes are used to define how your server should respond to different HTTP requests.
+ //request{
+      *get
+      *post
+      *put
+      *delete 
+    }
+  *routes is had a specific format to be a regexp in path-to-reg to use logices  
+  
+  *
+ */
+
+
